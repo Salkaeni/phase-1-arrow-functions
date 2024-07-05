47daaf4ba43b3dd40308be42c9db638c4cda6f75
Salome <salomekaeni@gmail.com>
2024-07-04 22:44:51 -0700

    add everything

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = (a, b) => {
+    if (b === 0) {
+      return "Error: Division by zero is not allowed.";
+    }
+    return a / b;
+  };
+  const square = x => x * x;
+  const add = (a, b) => a + b;
